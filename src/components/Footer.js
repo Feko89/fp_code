664d0688ac1351a2799ad879b4d499b4a3e37cd9
bukajis <petrilajakub123@gmail.com>
2025-02-04 22:40:42 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Footer.css';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from "./logo_fpcode.png";
 const Footer = () => (
     <footer>
-            <h1 class="footer-nadpis">FP CODE</h1>
-            <img src={logo} alt='logo' class="logo-foot"></img>
+            <div className='foot-inf'>
+           
             <div class="foot-con">
                 <div class="foot-grid">
                     <div class="foot-grid-dva">
@@ -35,6 +36,13 @@ const Footer = () => (
                         </div>
                     </div>
                 </div>
+            </div>
+            <Link to="/ucivo" className='pomoc'>
+                          <button className='but-fut'>
+                            Zacni sa ucit!
+                          </button>
+            </Link>
+            <img src={logo} alt='logo' class="logo-foot"></img>
             </div>
             <p class="copy-r">CopyRight@KUBKOADOMINIK S.R.O</p>
         </footer>);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Home.css'; // Pridaj CSS pre tento komponent
+import lampImage from './lampa.png'; // Zmenená cesta, keďže obrázok je v rovnakom priečinku
+import logo3d from './logo_python.png'; // Zmenená cesta, keďže obrázok je v rovnakom priečinku
+import spsepo from './spse-po.png'; // Zmenená cesta, keďže obrázok je v rovnakom priečink
+import arr from './arrow-removebg-preview.png'; // Zmenená cesta, keďže obrázok je v rovnakom priečinku
+import crown from './crown1-removebg-preview.png'; // Zmenená cesta, keďže obrázok je v rovnakom priečink
+import carpet from './carpez.png'; // Zmenená cesta, keďže obrázok je v rovnakom priečink
+
+
+
+
+
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(Boolean(localStorage.getItem("token")));
@@ -43,15 +54,38 @@ const Home = () => {
       </div>
 
       <div className="section-2">
+        <img src={lampImage} alt="lamp" className='lampa'/>
+        <img src={logo3d} alt="logo" className='python'/>
         <div className="onas-con">
           <div className="onas-text">
-            <h1 className="onas-nadpis">O tejto stranke</h1>
+            <h1 className="onas-nadpis">Prečo fp_code?</h1>
             <p className="onas-textik">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non dolor ligula...
+            Táto stránka je určená pre všetkých,<br></br>  ktorí sa chcú nadobudnúť základné informácie o <br></br> programovaní v Pythone prípadne Markdown. <br></br><br></br>Ponúka interaktívne návody, príklady kódu a priestor na<br></br> vlastné poznámky, aby ste sa mohli efektívne učiť<br></br> a rýchlo sa kedykoľvek vrátiť k dôležitým informáciám.
             </p>
           </div>
         </div>
       </div>
+      <div className="section-3">
+            <img src={carpet} alt="carpet" className='carpet'/>
+            <img src={crown} alt="crown" className='crown1'/>
+            <img src={crown} alt="crown" className='crown2'/>
+            <h1 className="ktosme-nadpis">Kto sme??</h1>
+            <div className='textiky'>
+            <div className="ktosme-textik1">
+            <h2>&lt;\Jakub Petrila&gt;</h2>
+            </div>
+            <img src={spsepo} alt="spse-po" className='spse-po'/>
+            <div className="ktosme-textik2">
+            <h2>&lt;\Dominik Fečo&gt;</h2>
+            </div>
+            </div>
+            <img src={arr} alt="arr" className='arr1'/>
+            <img src={arr} alt="arr" className='arr2'/>
+
+            <div className='text-kto'> Sme študenti SPŠE PO Prešov ktorý sa snažia v tento rok 2025 zmaturovať a odísť z tejto krásnej a milovanej školy ktorá nám bude chýbať.</div>
+          </div>
+        
+
     </>
   );
 };
